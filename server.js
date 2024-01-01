9736e25a29f9aff449c08623166f9d40da63942d
waiyan282 <waiyanhtun282@gmail.com>
2024-01-01 23:26:04 +0630

    finishghhz-get

--- server.js
+++ server.js
@@ -16,14 +16,19 @@ app.get("/users", async (req, res) => {
   //     { id: 2, name: "David " },
   //     { id: 3, name: "Stephn " },
   //   ];
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users = await response.json();
-  res.send(`
+  setTimeout(async () => {
+    const limit = +req.query.limit || 10;
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users?_limit=${limit}`
+    );
+    const users = await response.json();
+    res.send(`
    <h1 class='text-2xl   font-bold my-4'>Users</h1>
    <ul>
    ${users.map((user) => `<li>${user.name}</li>`).join("")}
    </ul>
   `);
+  }, 2000);
 });
 // Start the server
 app.listen(3000, () => {
