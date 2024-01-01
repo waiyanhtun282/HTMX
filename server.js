278e93c6c08230b3c173a2c65e7f0238c946c6ff
waiyan282 <waiyanhtun282@gmail.com>
2024-01-01 22:02:06 +0630

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from "express";
+
+const app = express();
+
+// set static folder
+app.use(express.static("public"));
+// Parse URL encoded bodies (as sent my HTML forms)
+app.use(express.urlencoded({ extended: true }));
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+// handle route get
+app.get("/users", async (req, res) => {
+  //   const users = [
+  //     { id: 1, name: "John " },
+  //     { id: 2, name: "David " },
+  //     { id: 3, name: "Stephn " },
+  //   ];
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
+  res.send(`
+   <h1 class='text-2xl   font-bold my-4'>Users</h1>
+   <ul>
+   ${users.map((user) => `<li>${user.name}</li>`).join("")}
+   </ul>
+  `);
+});
+// Start the server
+app.listen(3000, () => {
+  console.log("Server is listenting 3000");
+});
