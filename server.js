94a480e5b7fdc1bd12e7d5a7f21b5ce3a739b530
waiyan282 <waiyanhtun282@gmail.com>
2024-01-02 21:44:10 +0630

    finishtmxpolling

--- server.js
+++ server.js
@@ -46,6 +46,19 @@ app.post("/convert", (req, res) => {
     `);
   }, 2000);
 });
+// Handle Get Poll
+let counter =0;
+app.get('/poll',(req,res) =>{
+  counter++;
+  const data ={value:counter};
+  res.json(data)
+});
+// HandleGetTemeatuere for weather
+let currentTempearture =20;
+app.get('/get-temperature',(req,res) =>{
+  currentTempearture +=Math.random() * 2 -1;//Random temp change
+  res.send(currentTempearture.toFixed(1) + 'C')
+})
 // Start the server
 app.listen(3000, () => {
   console.log("Server is listenting 3000");
