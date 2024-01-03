234f53aa3cbb7dcc7235b96ed4c284a2a2387f53
waiyan282 <waiyanhtun282@gmail.com>
2024-01-03 15:23:05 +0630

    minipj

--- server.js
+++ server.js
@@ -47,18 +47,66 @@ app.post("/convert", (req, res) => {
   }, 2000);
 });
 // Handle Get Poll
-let counter =0;
-app.get('/poll',(req,res) =>{
+let counter = 0;
+app.get("/poll", (req, res) => {
   counter++;
-  const data ={value:counter};
-  res.json(data)
+  const data = { value: counter };
+  res.json(data);
 });
 // HandleGetTemeatuere for weather
-let currentTempearture =20;
-app.get('/get-temperature',(req,res) =>{
-  currentTempearture +=Math.random() * 2 -1;//Random temp change
-  res.send(currentTempearture.toFixed(1) + 'C')
-})
+let currentTempearture = 20;
+app.get("/get-temperature", (req, res) => {
+  currentTempearture += Math.random() * 2 - 1; //Random temp change
+  res.send(currentTempearture.toFixed(1) + "C");
+});
+
+// Handle POST
+const contacts = [
+  {
+    name: "Leo",
+    email: "leo@gmail.com",
+  },
+  {
+    name: "NeyMar",
+    email: "neymar@gmail.com",
+  },
+  {
+    name: "Stephan",
+    email: "stephan@gmail.com",
+  },
+];
+
+app.post('/search', (req, res) => {
+  const searchItem = req.body.search.toLowerCase();
+
+  if (!searchItem) {
+    return res.send('<tr></tr>');
+  }
+
+  const searchResults = contacts.filter((contact) => {
+    const name = contact.name.toLowerCase();
+    const email = contact.email.toLowerCase();
+    return (
+      name.includes(searchItem) || email.includes(searchItem)
+      );
+  });
+  setTimeout(() => {
+    const searchResultHtml = searchResults
+      .map(
+        (contact) => `
+    <tr>
+    <div class='my-4 p-2'>${contact.name}</div>
+    </tr>
+    <tr>
+    <div class='my-4 p-2'>${contact.email}</div>
+    </tr>
+
+    `
+      )
+      .join("");
+    res.send(searchResultHtml);
+  }, 1000);
+});
 // Start the server
 app.listen(3000, () => {
   console.log("Server is listenting 3000");
