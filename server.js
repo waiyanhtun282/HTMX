fbe264357cda165cfa59678c8dd9d2d86fc0cb17
Wai Yan <waiyanhtun282@gmail.com>
2024-01-07 18:40:55 +0630

    finishserachindex

--- server.js
+++ server.js
@@ -10,100 +10,96 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // handle route get
-app.get("/users", async (req, res) => {
+// app.get("/users", async (req, res) => {
   //   const users = [
   //     { id: 1, name: "John " },
   //     { id: 2, name: "David " },
   //     { id: 3, name: "Stephn " },
   //   ];
-  setTimeout(async () => {
-    const limit = +req.query.limit || 10;
-    const response = await fetch(
-      `https://jsonplaceholder.typicode.com/users?_limit=${limit}`
-    );
-    const users = await response.json();
-    res.send(`
-   <h1 class='text-2xl   font-bold my-4'>Users</h1>
-   <ul>
-   ${users.map((user) => `<li>${user.name}</li>`).join("")}
-   </ul>
-  `);
-  }, 2000);
-});
+//   setTimeout(async () => {
+//     const limit = +req.query.limit || 10;
+//     const response = await fetch(
+//       `https://jsonplaceholder.typicode.com/users?_limit=${limit}`
+//     );
+//     const users = await response.json();
+//     res.send(`
+//    <h1 class='text-2xl   font-bold my-4'>Users</h1>
+//    <ul>
+//    ${users.map((user) => `<li>${user.name}</li>`).join("")}
+//    </ul>
+//   `);
+//   }, 2000);
+// });
 
 // Handle post route
-app.post("/convert", (req, res) => {
-  setTimeout(() => {
-    const fahrenheit = parseFloat(req.body.fahrenheit);
-    const celisus = (fahrenheit - 32) * (5 / 9);
+// app.post("/convert", (req, res) => {
+//   setTimeout(() => {
+//     const fahrenheit = parseFloat(req.body.fahrenheit);
+//     const celisus = (fahrenheit - 32) * (5 / 9);
 
-    res.send(`
-    <p>
-    ${fahrenheit} degree Fahernheit is equal to ${celisus.toFixed(
-      2
-    )} degrees Celisus
-    </p>
-    `);
-  }, 2000);
-});
+//     res.send(`
+//     <p>
+//     ${fahrenheit} degree Fahernheit is equal to ${celisus.toFixed(
+//       2
+//     )} degrees Celisus
+//     </p>
+//     `);
+//   }, 2000);
+// });
 // Handle Get Poll
-let counter = 0;
-app.get("/poll", (req, res) => {
-  counter++;
-  const data = { value: counter };
-  res.json(data);
-});
+// let counter = 0;
+// app.get("/poll", (req, res) => {
+//   counter++;
+//   const data = { value: counter };
+//   res.json(data);
+// });
 // HandleGetTemeatuere for weather
-let currentTempearture = 20;
-app.get("/get-temperature", (req, res) => {
-  currentTempearture += Math.random() * 2 - 1; //Random temp change
-  res.send(currentTempearture.toFixed(1) + "C");
-});
+// let currentTempearture = 20;
+// app.get("/get-temperature", (req, res) => {
+//   currentTempearture += Math.random() * 2 - 1; //Random temp change
+//   res.send(currentTempearture.toFixed(1) + "C");
+// });
 
 // Handle POST
-const contacts = [
-  {
-    name: "Leo",
-    email: "leo@gmail.com",
-  },
-  {
-    name: "NeyMar",
-    email: "neymar@gmail.com",
-  },
-  {
-    name: "Stephan",
-    email: "stephan@gmail.com",
-  },
-];
+// const contacts = [
+//   { name: 'John Doe', email: 'john@example.com' },
+//   { name: 'Jane Doe', email: 'jane@example.com' },
+//   { name: 'Alice Smith', email: 'alice@example.com' },
+//   { name: 'Bob Williams', email: 'bob@example.com' },
+//   { name: 'Mary Harris', email: 'mary@example.com' },
+//   { name: 'David Mitchell', email: 'david@example.com' },
+// ];
 
-app.post('/search', (req, res) => {
-  const searchItem = req.body.search.toLowerCase();
-
-  if (!searchItem) {
+// Handle POST request for contacts search
+app.post('/search',  async (req, res) => {
+  const searchTerm = req.body.search.toLowerCase();
+ const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users`
+    );
+    const contacts = await response.json()
+  if (!searchTerm) {
     return res.send('<tr></tr>');
   }
 
   const searchResults = contacts.filter((contact) => {
     const name = contact.name.toLowerCase();
     const email = contact.email.toLowerCase();
-    return (
-      name.includes(searchItem) || email.includes(searchItem)
-      );
+
+    return name.includes(searchTerm) || email.includes(searchTerm);
   });
+
   setTimeout(() => {
     const searchResultHtml = searchResults
       .map(
         (contact) => `
-    <tr>
-    <div class='my-4 p-2'>${contact.name}</div>
-    </tr>
-    <tr>
-    <div class='my-4 p-2'>${contact.email}</div>
-    </tr>
-
+      <tr>
+        <td><div class="my-4 p-2">${contact.name}</div></td>
+        <td><div class="my-4 p-2">${contact.email}</div></td>
+      </tr>
     `
       )
-      .join("");
+      .join('');
+
     res.send(searchResultHtml);
   }, 1000);
 });
